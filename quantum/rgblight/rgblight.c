e5491cfb2f11fe900944b0f47949de09e4645484
Joshua Diamond <josh@windowoffire.com>
2020-05-14 10:15:49 -0400

    Add O3 for Twinkle effect implementation

--- quantum/rgblight/rgblight.c
+++ quantum/rgblight/rgblight.c
@@ -1352,6 +1352,8 @@ void rgblight_effect_alternating(animation_status_t *anim) {
 #endif
 
 #ifdef RGBLIGHT_EFFECT_TWINKLE
+#pragma GCC push_options
+#pragma GCC optimize ("O3")
 __attribute__((weak)) const uint8_t RGBLED_TWINKLE_INTERVALS[] PROGMEM = {30, 15, 5};
 
 typedef struct PACKED {
@@ -1415,6 +1417,7 @@ void rgblight_effect_twinkle(animation_status_t *anim) {
 
     rgblight_set();
 }
+#pragma GCC pop_options
 #endif
 
 void preprocess_rgblight(void) {
