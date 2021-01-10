6d8ea4055660f8cfdbd503dc8fb59ea3b8482476
Joshua Diamond <josh@windowoffire.com>
2021-01-09 23:31:49 -0500

    xt_usb keymap for spidey3

---
+++ keyboards/converter/xt_usb/keymaps/spidey3/rules.mk
@@ -0,0 +1,13 @@
+# Build Options
+#   comment out to disable the options.
+#
+MOUSEKEY_ENABLE = no	# Mouse keys
+CONSOLE_ENABLE = yes	# Console for debug
+COMMAND_ENABLE = no
+BACKLIGHT_ENABLE = no	# Enable keyboard backlight functionality
+UNICODEMAP_ENABLE = yes
+VELOCIKEY_ENABLE = yes
+GRAVE_ESC_ENABLE = no
+
+# The following disabled to save space
+SPACE_CADET_ENABLE = no
